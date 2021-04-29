2ef9d8c30c92a1beb2e7cc5829a0777026c4308f
Nikolas North <nikolasn09@gmail.com>
2021-04-29 19:29:42 -0400

    create server and allow graceful shutdown upon SIGINT

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/nikolasnorth/microservices/handlers"
 )
@@ -19,9 +22,37 @@ func main() {
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
 
-	log.Printf("listening on port %s...", PORT)
-	err := http.ListenAndServe(":"+PORT, sm)
+	l.Printf("listening on port %s...", PORT)
+
+	s := &http.Server{
+		Addr:         ":" + PORT,
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	// Listen for SIGINT signal
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	// Will block until SIGINT happens
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	// Allow 30s grace period before shutting down server. Accept no new connections during this time.
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	err := s.Shutdown(ctx)
 	if err != nil {
-		log.Fatal(err)
+		l.Fatal(err)
 	}
 }
